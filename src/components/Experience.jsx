062bf47e8c2651be94229e9b6e13776505eb5a00
wsilvadev <wsilvadev@gmail.com>
2023-12-28 22:40:41 -0300

    generate animation to canvas

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,17 @@
 import { OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import React, { Suspense } from "react";
 
 export const Experience = () => {
   return (
     <>
+      <ambientLight intensity={1} />
+      <group position-y={-1}>
+         <Avatar/>
+
+      </group>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      
     </>
   );
 };
